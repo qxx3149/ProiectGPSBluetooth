79496f974ef3ef40cffe070a4465edb467201fea
Parasca Alexandru <48731208+qxx3149@users.noreply.github.com>
2019-05-04 20:40:44 +0300

    Start code for bluetooth communication.

--- GPSBluetooth.ino
+++ GPSBluetooth.ino
@@ -14,21 +14,38 @@ TinyGPSPlus gps;
 
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
+// The serial connection to the Bluetooth device
+SoftwareSerial BlueTooth(11,10);
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
+  while (!Serial) 
+  {
+     // wait for serial port to connect. Needed for native USB port only
+  }
   ss.begin(GPSBaud);
+  BlueTooth.begin(9600);
 }
 
-void loop(){
+void loop()
+{
   // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0){
+  ss.listen();
+  if (ss.available() > 0)
+  {
     gps.encode(ss.read());
-    if (gps.location.isUpdated()){
+    if (gps.location.isUpdated())
+    {
       Serial.print("Latitude= "); 
       Serial.print(gps.location.lat(), 6);
       Serial.print(" Longitude= "); 
       Serial.println(gps.location.lng(), 6);
     }
+  }
+  //BlueTooth.listen();
+  if(BlueTooth.available() > 0)
+  {
+    Serial.println(" Salut"); 
   }
 }
